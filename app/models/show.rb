13a4b2dab2088f5e4498b9c8b99da273c8db7a8a
Brian <mahuibrian14@gmail.com>
2023-06-05 23:25:35 +0300

    Done

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,17 @@
 class Show < ActiveRecord::Base
+    belongs_to :network
+    has_many :characters
+    has_many :actors, through: :characters
   
+    def characters
+      Character.where(show_id: id)
+    end
+  
+    def network
+      Network.find(network_id)
+    end
+  
+    def actors_list
+      actors.pluck(:full_name)
+    end
 end
